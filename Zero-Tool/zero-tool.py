1b5edf2a0cb61efb1585cda1db4cc121a926f2cc
gzyomi <128975697+gzyomi@users.noreply.github.com>
2023-09-21 22:03:04 +0200

    Update zero-tool.py

--- Zero-Tool/zero-tool.py
+++ Zero-Tool/zero-tool.py
@@ -1,7 +1,7 @@
 import sys,os
 from colorama import Fore
 
-print(Fore.MAGENTA+"""
+print(Fore.RED+"""
 
 ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
 MADE BY POLAR
