9da5d625007e3e098d4e5f5b381817f2f799a705
gzyomi <128975697+gzyomi@users.noreply.github.com>
2023-09-22 21:27:43 +0200

    Update ip-lookup.py

--- Zero-Tool/ip-lookup.py
+++ Zero-Tool/ip-lookup.py
@@ -11,16 +11,16 @@ def phone_locator(phone_number, api_key):
 
 def print_phone_details(phone_details):
     print("Phone Details:")
-    print(f"Number: {phone_details['number']}")
-    print(f"Country: {phone_details['country_name']} ({phone_details['country_code']})")
-    print(f"Carrier: {phone_details['carrier']}")
-    print(f"Line Type: {phone_details['line_type']}")
-    print(f"Location: {phone_details['location']}")
-    print(f"Valid: {phone_details['valid']}")
+    print(f"Numero: {phone_details['number']}")
+    print(f"Regione: {phone_details['country_name']} ({phone_details['country_code']})")
+    print(f"Carriera: {phone_details['carrier']}")
+    print(f"Linea: {phone_details['line_type']}")
+    print(f"Posizione: {phone_details['location']}")
+    print(f"Valid*: {phone_details['valid']}")
 
 api_key = "22WfDqB635jMOWkxdeC1r2BXew1tXHLU"
 
-phone_number = input(Fore.MAGENTA+"Enter the phone number to locate: ")
+phone_number = input(Fore.CYAN+Scrivi il numero di telefono per localizzarlo: ")
 
 phone_details = phone_locator(phone_number, api_key)
 print_phone_details(phone_details)
